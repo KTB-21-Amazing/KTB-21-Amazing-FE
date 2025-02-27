4560ecdf0eba3b9319980fdbf02e382254d3f2d1
lunghyun <nangmannsh@gmail.com>
2025-02-28 08:07:17 +0900

    mod: test용 IP or 연산 추가

--- AMAZING-/src/api/instance.ts
+++ AMAZING-/src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL as string,
+  baseURL: process.env.REACT_APP_URL as string || 'http://192.168.40.177:8000',
   headers: {
     'Content-Type': 'application/json',
   },
