d5703450fa43d8d155130598634eaf042035bb50
lunghyun <nangmannsh@gmail.com>
2025-02-28 08:23:16 +0900

    mod: API URL 환경변수 as String 제거

--- AMAZING-/src/api/instance.ts
+++ AMAZING-/src/api/instance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_URL as string || 'http://192.168.40.177:8000';
+const baseURL = process.env.REACT_APP_URL;
 console.log('REACT_APP_URL:', baseURL);
 
 const api = axios.create({
