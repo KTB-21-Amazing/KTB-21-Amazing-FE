a0700f3ec8ce9a8201dfb776babc8b08d5b08b06
lunghyun <nangmannsh@gmail.com>
2025-02-28 08:21:02 +0900

    mod: instance.ts 디버깅

--- AMAZING-/src/api/instance.ts
+++ AMAZING-/src/api/instance.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_URL as string || 'http://192.168.40.177:8000';
+console.log('REACT_APP_URL:', baseURL);
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_URL as string || 'http://192.168.40.177:8000',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
