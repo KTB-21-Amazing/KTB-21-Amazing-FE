3e8f45b37bf489ee990307a47dec798d626a5a18
lunghyun <nangmannsh@gmail.com>
2025-02-28 08:48:06 +0900

    mod: url 타입 확인

--- AMAZING-/src/api/instance.ts
+++ AMAZING-/src/api/instance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_URL as string || 'http://192.168.40.177:8000';
+console.log('REACT_APP_URL:', typeof process.env.REACT_APP_URL);
 console.log('REACT_APP_URL:', baseURL);
 
 const api = axios.create({
